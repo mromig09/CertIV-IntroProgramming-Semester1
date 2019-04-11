a97df13cb1112324d4371bf8fe7701519128f059
mromig09 <mromig09@users.noreply.github.com>
2019-04-11 17:04:48 +1000

    Arrays

---
+++ Arrays and Lists/Arrays and Lists/Using Arrays 1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Arrays_and_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                        
+            int i = 0;
+            int count = 0;
+            string[] names = new string[5];
+
+            while (count < names.Length)
+            {
+                Console.WriteLine("Enter Name: ");
+                names[count] = Console.ReadLine();
+                count++;
+            }
+
+            while (i < names.Length)
+            {
+                Console.Write(" Hello " + names[i]);
+                i++;
+            }
+           
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Arrays and Lists/Using Arrays2/Using Arrays 2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Using_Arrays2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("34, 5, 67, 1, 99, 34, 44, 78, 34, 0"); 
+            
+            int[] arr = new int[] { 34, 5, 67, 1, 99, 34, 44, 78, 34, 0 };
+            int sum = 0;
+            int i = 0;
+
+            while (i < arr.Length)
+            {
+                sum = sum + arr[i];
+                i++;  
+            }
+            Console.WriteLine(sum);
+
+            Console.ReadKey(true);
+        }
+    }
+}
