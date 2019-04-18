71e3c8c6e261bbc76b5ec680dcc674b54572f1fb
mromig09 <mromig09@users.noreply.github.com>
2019-04-18 18:47:27 +1000

    Lists

---
+++ Lists/Lists 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> iList = new List<int>();
+            iList.Add(9);
+            iList.Add(9);
+            iList.Add(6);
+            iList.Add(10);
+            iList.Add(9);
+
+            foreach (object i in iList)
+            {
+                Console.Write(i);
+            }
+
+            Console.WriteLine(" Enter another number");
+            int input = int.Parse(Console.ReadLine());
+            
+            int count = 0;
+
+            for (int i = 0; i < 0; i++)
+            Console.WriteLine();
+
+            if (count > 0)
+
+            {
+                Console.WriteLine("The number " + input + " already exists");
+                int index = iList.IndexOf(item: 9, index: 1);
+            }
+            else
+            {
+                Console.WriteLine("Number doesn't exist in the list");
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Lists/Lists/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> iList = new List<int>();
+            iList.Add(9);
+            iList.Add(7);
+            iList.Add(6);
+            iList.Add(10);
+            iList.Add(20);
+
+            foreach (object i in iList)
+            {
+                Console.Write(i);
+            }
+
+            Console.WriteLine(" Enter another number");
+            int input = int.Parse(Console.ReadLine());
+            if (iList.Contains(input))           
+            {
+                Console.WriteLine("The number "+input+" already exists");
+            } else
+            {
+                Console.WriteLine("Number doesn't exist in the list");
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
