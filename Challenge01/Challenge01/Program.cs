ca5c0706b0de097bd1738cc8970099ba64c96cb0
mromig09 <mromig09@users.noreply.github.com>
2019-05-06 13:34:32 +1000

    Update Program.cs

--- Challenge01/Challenge01/Program.cs
+++ Challenge01/Challenge01/Program.cs
@@ -12,70 +12,71 @@ namespace Challenge01
         {
             //array "Currently "wrong" must fix
             int[] values = new int[6];
-            for (int i = 0; i < 7; i++) 
+            for (int i = 0; i < 7; i++)
 
-            /*
-            values[0] = 1;
-            values[1] = 2;
-            values[2] = 3;
-            values[3] = 4;
-            values[4] = 5;
-            values[5] = 6; */
+                /*
+                values[0] = 1;
+                values[1] = 2;
+                values[2] = 3;
+                values[3] = 4;
+                values[4] = 5;
+                values[5] = 6; */
 
-            //statement loop
-            for ( ; ; )
-            {
+                //statement loop
+                for (; ; )
+                {
                     //Welcoming line >>need to input "roll" command<<
                     int userInput;
-                Console.WriteLine("Let's Play a Game of Dice!");
-                Console.WriteLine("Roll the dice");
-                userInput = Console.Read();
+                    Console.WriteLine("Let's Play a Game of Dice!");
+                    Console.WriteLine("Roll the dice");
+                    userInput = Console.Read();
 
-                //create a new dice object and roll
-                Dice newRun = new Dice();
-                newRun.Roll();
+                    //create a new dice object and roll
+                    Dice newRun = new Dice();
+                    newRun.Roll();
 
-                //display the results and line space
-                Console.WriteLine("You Rolled a {0}", newRun.Die1);
-                Console.WriteLine();
+                    //display the results and line space
+                    Console.WriteLine("You Rolled a {0}", newRun.Die1);
+                    Console.WriteLine();
 
-                //play again
-                Console.WriteLine("Would you like to roll again?");
-                Console.ReadLine();
+                    //play again
+                    Console.WriteLine("Would you like to roll again?");
+                    Console.ReadLine();
 
-                //if yes we continue, if no cya, bye.
-                string yes = Console.ReadLine();
-                {
-                    if (yes.Equals("yes"))
+                    //if yes we continue, if no cya, bye.
+                    string yes = Console.ReadLine();
                     {
-                        Console.WriteLine("Lets Play Again. . . ");
-                        Console.WriteLine();
+                        if (yes.Equals("yes"))
+                        {
+                            Console.WriteLine("Lets Play Again. . . ");
+                            Console.WriteLine("Push Enter to Start. . .");
+                            Console.WriteLine();
+                        }
+                        else if (yes.Equals("no"))
+                        {
+                            Console.WriteLine("Cya, Bye. . .");
+                        }
+                        Console.ReadKey();
                     }
-                    else if (yes.Equals("no"))
-                    {
-                        Console.WriteLine("Cya, Bye. . .");                        
-                    }
-                }                 
-                Console.ReadKey();
+                }
+        }
+
+        class Dice
+        {
+            private int die1;
+
+            public int Die1
+            {
+                get { return die1; }
+                set { die1 = value; }
             }
-        }
-    }
-    
-    class Dice
-    {
-        private int die1;
 
-        public int Die1
-        {
-            get { return die1; }
-            set { die1 = value; }
-        }
-        
-        public void Roll()
-        {
-            //generate a number for the die face
-            Random rand1 = new Random();
-            Die1 = rand1.Next(1, 6);
+            public void Roll()
+            {
+                //generate a number for the die face
+                Random rand1 = new Random();
+                Die1 = rand1.Next(1, 6);
+            }
         }
     }
 }
