55a9b96a1184e1e01ce3c003c41cf23ccc8f3aa0
mromig09 <mromig09@users.noreply.github.com>
2019-06-13 15:12:51 +1000

    inheritance

---
+++ inheritance/inheritance/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    public abstract class Animal
+    {
+
+        public string Name { get; set; }
+
+        public float Fatness { get; set; }
+
+        public int Age { get; set; }
+
+        public Animal(string name)
+        {
+            this.Name = name;
+        }
+
+        public abstract void Move();
+        public abstract void Talk();
+    }
+
+    public class Dog : Animal
+    {
+        public Dog(string breed, string ownerName) : base("Dog")
+        { 
+            Console.WriteLine("Dog Construct");
+            Breed = breed;
+            OwnerName = OwnerName;
+        }
+        public string Breed { get; set; }
+        public string OwnerName { get; set; }
+    
+        public override void Move()
+        {
+            Console.WriteLine("runnningggggg");
+        }
+
+        public override void Talk()
+        {
+            Console.WriteLine("Woof Woof!!");
+        }
+    }
+}
---
+++ inheritance/inheritance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            Dog myDog = new Dog("Staffy", "Mark");
+            myDog.Move();
+
+            Console.ReadLine();
+        }
+    }
+}
